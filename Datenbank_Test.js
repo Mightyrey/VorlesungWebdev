55b108e6608c1cdf90304f6f50d944673e1a3e14
Mightyrey <tobias.hartmann297@gmail.com>
2020-09-18 14:09:56 +0200

    test

---
+++ Datenbank_Test.js
@@ -0,0 +1,25 @@
+//Erstellen einer Datenbank
+
+var mysql = require('mysql');
+
+
+const { createConnection } = require('net');
+
+
+var con = mysql.createConnection({
+  // Datenbank Einstellungen
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  /*Create a database named "mydb":*/
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+ });
+});
+
---
+++ Programm.js
@@ -0,0 +1,37 @@
+console.log('Test');
+console.log('12.0');
+
+
+for(let i=0; i<=100;i++)
+{
+    let Teilbar3 = i;
+    Teilbar3 = i % 3;
+    let Teilbar5;
+    Teilbar5 = i % 5;
+
+    if(Teilbar3==0 && Teilbar5 == 0)
+    {
+        console.log('Zahl: FizzBuzz')
+    }
+    else
+    {
+        if(Teilbar3==0)
+        {
+            console.log('Zahl: Fizz')
+        }
+
+        if(Teilbar5==0)
+        {
+            console.log('Zahl: Buzz')
+
+    }
+    else{
+        console.log('Zahl:'+ i);
+    }
+
+        }
+
+}
+
+
+          
